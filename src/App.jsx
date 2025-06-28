f7a45b0608c97c19549360a51f51daa1a3e8247e
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-28 08:42:03 +0100

    Fetching promise done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
 import './App.css'
 import Countries from './components/countries/countries/'
+const countriesPromise = fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json());
 function App() {
   return (
     <>
-    <Countries></Countries> 
+      <Suspense fallback="Loading...">
+        <Countries></Countries>
+      </Suspense>
+
     </>
   )
 }
-
 export default App
