b3b6652fba37b1ed6b91c514da1a2d2f10932ea0
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-29 20:37:12 +0100

    Lift up the state in arrays by Comparing arrays

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -5,11 +5,18 @@ const  Countries =({ countriesPromise })=> {
     const [visitedCountries,setVisitedCoutries] = useState([]);
     console.log(countries);
     const handleVisitedCountries = (){
-        console.log('country visited clicked to be added');
+        console.log('country visited clicked to be added',country);
+        const newVisitedCountries = [...visitedCountries];
+        setVisitedCoutries(newVisitedCountries);
     }
     return (
         <>
          <h3>Travelled So Far</h3>
+         <ol>
+            {
+                visitedCountries.map = (country => <li>{country.name.common}</li>)
+            }
+         </ol>
         <div>
             {
                 countries.map(country => < Country key={country.cca3} handleVisitedCountries={handleVisitedCountries} country={country} ></Country>)
